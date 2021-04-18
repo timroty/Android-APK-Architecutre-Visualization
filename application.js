363143f3307a3e732da9815c2a5ea452d16c7908
Laura Derowitsch <derowitschlaura@gmail.com>
2021-04-18 11:48:43 -0500

    set interface coordinates

--- application.js
+++ application.js
@@ -6,27 +6,27 @@ const dir = './Messenger.xml'
 let middleObject = {};
 
 let outputObject = {
-	xADL: {
-		structure: {
-			component: []
+    xADL: {
+        structure: {
+            component: []
         },
         "_xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-		"_xmlns:hints_3_0": "http://www.archstudio.org/xadl3/schemas/hints-3.0.xsd",
-		"_xmlns:structure_3_0": "http://www.archstudio.org/xadl3/schemas/structure-3.0.xsd",
-		"_xmlns:xadlcore_3_0": "http://www.archstudio.org/xadl3/schemas/xadlcore-3.0.xsd",
-		"__prefix": "xadlcore_3_0"
+        "_xmlns:hints_3_0": "http://www.archstudio.org/xadl3/schemas/hints-3.0.xsd",
+        "_xmlns:structure_3_0": "http://www.archstudio.org/xadl3/schemas/structure-3.0.xsd",
+        "_xmlns:xadlcore_3_0": "http://www.archstudio.org/xadl3/schemas/xadlcore-3.0.xsd",
+        "__prefix": "xadlcore_3_0"
     }
 }
 
 
 
 async function readCovertFile() {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         let covertFile = undefined;
 
-        fs.readFile(dir, 'utf8', function (error, data){
+        fs.readFile(dir, 'utf8', function(error, data) {
             if (error) {
-            reject(error);
+                reject(error);
             }
             covertFile = data;
 
@@ -37,17 +37,16 @@ async function readCovertFile() {
     });
 }
 
-async function buildArchStudioObject () {
+async function buildArchStudioObject() {
 
     var keys = Object.keys(middleObject.components);
-    
-    for(let i in keys){
+
+    for (let i in keys) {
 
         let tempComponent = {
             "interface": [],
             "ext": {
-                "hint": [
-                    {
+                "hint": [{
                         "_hints_3_0:hint": "org.eclipse.swt.graphics.Rectangle:" + middleObject.components[keys[i]].x_coord + "," + middleObject.components[keys[i]].y_coord + "," + middleObject.components[keys[i]].width + "," + middleObject.components[keys[i]].height,
                         "_hints_3_0:name": "bounds",
                         "__prefix": "hints_3_0"
@@ -66,12 +65,29 @@ async function buildArchStudioObject () {
             '_structure_3_0:name': middleObject.components[keys[i]].name,
         }
 
-        for(let j = 0; j < middleObject.components[keys[i]].interface.length; j ++){
+        for (let j = 0; j < middleObject.components[keys[i]].interface.length; j++) {
             // TODO CHANGE THE JS POINT 2D
+
+            let x = 0;
+            let y = 0;
+
+            // set coordinates for in and out interfaces
+            if (middleObject.components[keys[i]].interface[j].direction == 'in') {
+
+                x = 0;
+                y = 0;
+
+            } else if (middleObject.components[keys[i]].interface[j].direction == 'out') {
+
+                x = 4651444358887768064;
+                y = 4651444358887768064;
+
+            }
+
             let interfaceObject = {
                 "ext": {
                     "hint": {
-                        "_hints_3_0:hint": "java.awt.geom.Point2D:1,0,0",
+                        "_hints_3_0:hint": "java.awt.geom.Point2D:1," + x + "," + y,
                         "_hints_3_0:name": "location",
                         "__prefix": "hints_3_0"
                     },
@@ -88,7 +104,7 @@ async function buildArchStudioObject () {
         }
 
         outputObject.xADL.structure.component.push(tempComponent)
-    }		
+    }
 }
 
 async function parseCovertFile() {
@@ -105,31 +121,31 @@ async function parseCovertFile() {
     const WIDTH = 100;
     const OFFSET = 25;
     const NUM_WIDE = 4;
-    
+
     let componentNum = 1;
     jsonComponents.forEach(element => {
-        
+
         let name = element["elements"][1]["elements"][0]["text"]
         let type = element["elements"][0]["elements"][0]["text"]
         let id = "componentId" + componentNum;
-        
-        let x = ((componentNum * WIDTH) + (componentNum * OFFSET)) % ((WIDTH + OFFSET) * NUM_WIDE);   
+
+        let x = ((componentNum * WIDTH) + (componentNum * OFFSET)) % ((WIDTH + OFFSET) * NUM_WIDE);
         let y = (HEIGHT + OFFSET) * Math.floor(componentNum / NUM_WIDE);
 
         var component = {
-            "name" : name,
-            "type" : type,
-            "id" : id, 
-            "x_coord" : x,
-            "y_coord" : y,
-            "height" : HEIGHT,
-            "width" : WIDTH
+            "name": name,
+            "type": type,
+            "id": id,
+            "x_coord": x,
+            "y_coord": y,
+            "height": HEIGHT,
+            "width": WIDTH
         }
 
         middleObject.components[name] = component
         middleObject.components[name].interface = [];
 
-        componentNum ++;
+        componentNum++;
     });
 
     let interfaceNum = 1;
@@ -143,19 +159,19 @@ async function parseCovertFile() {
         let linkId = "linkId" + interfaceNum;
 
         var sendInterface = {
-            direction : "out",
-            id : senderInterfaceId
+            direction: "out",
+            id: senderInterfaceId
         }
 
         var componentInterface = {
-            direction : "in",
-            id : componentInterfaceId
+            direction: "in",
+            id: componentInterfaceId
         }
 
         var linkInformation = {
-            id : linkId,
-            interface1 : senderInterfaceId,
-            interface2 : componentInterfaceId
+            id: linkId,
+            interface1: senderInterfaceId,
+            interface2: componentInterfaceId
         }
 
         middleObject.components[sender].interface.push(sendInterface);
@@ -163,8 +179,8 @@ async function parseCovertFile() {
 
         middleObject.links.push(linkInformation);
 
-        interfaceNum ++;
-        
+        interfaceNum++;
+
     });
 }
 
@@ -172,7 +188,7 @@ async function main() {
     await parseCovertFile();
 
     buildArchStudioObject();
-    
+
     //ar keys = Object.keys(middleObject.components);
 
     // for(let i in keys){
@@ -184,6 +200,3 @@ async function main() {
 }
 
 main();
-
-  
-
